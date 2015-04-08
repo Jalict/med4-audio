a967c1f39c5d8696a5a16956b117c2c722264407
Frans Peter Larsen <larsen.frans@gmail.com>
2015-04-08 17:56:56 +0200

    Radio tower sounds and player ambience

---
+++ Assets/Scripts/AmbienceMover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbienceMover : MonoBehaviour {
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 t = player.transform.position;
+        t.y += 0.1f;
+        t.z += 0.38f;
+        transform.position = t;
+
+	}
+}
