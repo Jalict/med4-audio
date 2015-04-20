7be86184d23c456b0233aff2f6d0f7c980b41a2b
Frans Peter Larsen <larsen.frans@gmail.com>
2015-04-20 11:35:44 +0200

    Late changes

---
+++ Assets/Scripts/ToggleMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleMovement : MonoBehaviour {
+    public OtherMovementSystem other;
+
+    public Material skybox1;
+    public Material skybox2;
+    public Material skybox3;
+
+    public Light light;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        light.intensity = 0.20f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            other.enabled = !other.enabled;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            RenderSettings.skybox = skybox1;
+            light.intensity = 0.05f;;
+        } 
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            RenderSettings.skybox = skybox2;
+            light.intensity = 0.125f;
+        } 
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            RenderSettings.skybox = skybox3;
+            light.intensity = 0.20f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            light.intensity = 1f;
+        }
+	}
+}
---
+++ Assets/Scripts/Tracker.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public class Tracker : MonoBehaviour {
+    private Camera cam;
+    public GameObject track;
+    public Light dirLight;
+
+    private string filename;
+
+    private bool isTracking;
+
+    private StreamWriter file;
+    private Texture2D shot;
+
+
+	// Use this for initialization
+	void Start () {
+        cam = gameObject.GetComponent<Camera>();
+
+        filename = System.DateTime.Now.ToString("dd-MM-yyyy_HH-mm");
+
+        if(File.Exists(filename + ".txt")) {
+            Debug.Log("SHIT EXIST");
+            return;
+        }
+        file = File.CreateText(filename+".txt");
+
+        StartTracking();
+
+        StartCoroutine(GetInitialOverviewTexture());
+	}
+
+    IEnumerator Tracking()
+    {
+        while (isTracking)
+        {
+            file.WriteLine(track.transform.position.ToString());
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void StartTracking()
+    {
+        isTracking = true;
+        StartCoroutine(Tracking());
+    }
+
+    void StopTracking()
+    {
+        isTracking = false;
+        StopCoroutine(Tracking());
+    }
+
+    IEnumerator GetInitialOverviewTexture()
+    {
+        dirLight.intensity = 1f;
+
+        yield return new WaitForEndOfFrame();
+
+        cam.targetTexture = new RenderTexture(1024, 1024, 24);
+
+        shot = new Texture2D(1024, 1024, TextureFormat.RGB24, false);
+
+        cam.Render();
+
+        RenderTexture.active = cam.targetTexture;
+
+        shot.ReadPixels(new Rect(0, 0, 1024, 1024), 0, 0);
+
+        cam.targetTexture = null;
+        RenderTexture.active = null;
+
+        dirLight.intensity = 0.20f;
+
+        // DO REST HERE:
+        // http://answers.unity3d.com/questions/22954/how-to-save-a-picture-take-screenshot-from-a-camer.html
+    }
+
+    void OnApplicationQuit()
+    {
+        StopCoroutine(Tracking());
+        file.Close();
+
+        byte[] bytes = shot.EncodeToPNG();
+        System.IO.File.WriteAllBytes(filename + ".png", bytes);
+        Debug.Log(string.Format("Took screenshot to: {0}", filename + ".png"));
+    }
+}
+
