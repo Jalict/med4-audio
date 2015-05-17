f0086069491f45e286b1c8cc561ffa9647fd50c3
Hethix <An.ryge@gmail.com>
2015-05-17 15:22:29 +0200

    All sounds finished
    Only missing fix for movement and water

---
+++ Med4-407-Final-Iteration/Assets/Scripts/LightController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightController : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        gameObject.GetComponent<Light>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.L))
+            gameObject.GetComponent<Light>().enabled = !gameObject.GetComponent<Light>().enabled;
+	}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 170, 30), "Press L to Toggle Night");
+    }
+}
---
+++ Med4-407-Final-Iteration/Assets/Scripts/SoundControl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using TBE_3DCore;
+
+public class SoundControl : MonoBehaviour {
+	public static SoundControl instance;
+	public int state; // 0 = Control, 1 = Amplitude
+
+
+	// Use this for initialization
+	void Start () {
+		state = 0;
+	}
+
+	void AmplitudeChange(GameObject[] obj) {
+	}
+
+	void Awake() {
+		if(instance)
+			DestroyImmediate(gameObject);
+		else {
+			instance = this;
+			DontDestroyOnLoad(instance);
+		}
+
+		StartCoroutine(GoThroughTags());
+	}
+
+	public IEnumerator GoThroughTags() {
+		yield return new WaitForEndOfFrame();
+
+		GameObject[] soundsObjs = GameObject.FindGameObjectsWithTag("SoundChange");
+
+		foreach(GameObject obj in soundsObjs) {
+			if(state == 1) {
+				obj.GetComponent<TBE_Source>().minimumDistance = 9999;
+			}
+		}
+	}
+
+	public void StartIterator() {
+		StartCoroutine(GoThroughTags());
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Alpha1)) {
+			state = 1;
+			StartCoroutine(GoThroughTags());
+		}
+		if(Input.GetKeyDown(KeyCode.K)) {
+			Application.LoadLevel(1);
+		}
+	}
+}
